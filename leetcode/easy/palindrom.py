0972cbe868a001473fa78786994cd8ce6ed453df
himanshush200599 <himanshush214@gmail.com>
2018-09-22 16:07:55 -0400

    First problem in leetcode

---
+++ leetcode/easy/palindrom.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        if x == x[::-1]:
+            return True
+        return False

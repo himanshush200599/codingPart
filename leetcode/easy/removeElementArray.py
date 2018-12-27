9beac875a5c8a5bdca7f9c34de4b95bcfd0e6fa1
himanshush200599 <himanshush214@gmail.com>
2018-12-27 10:12:23 -0500

    remove element from array

---
+++ leetcode/easy/removeElementArray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        n =len(nums)
+        while i<n:
+            if nums[i] == val:
+                nums[i] = nums[n-1]
+                n-=1
+            else:
+                i+=1
+        return n

97b637f82bab2b1761ada349c2872a981d8e618c
himanshush200599 <himanshush214@gmail.com>
2019-01-13 03:05:46 -0500

    added 2more methods one from hash table and other using binary search

--- leetcode/easy/TwoElemInArraySumEqualToTarget.py
+++ leetcode/easy/TwoElemInArraySumEqualToTarget.py
@@ -15,3 +15,27 @@ class Solution(object):
             else:
                 r -= 1
 # binary search solution in nlogn Sorted array
+
+
+#others  two  methods are as follows
+        # dictionary
+def twoSum2(self, numbers, target):
+    dic = {}
+    for i, num in enumerate(numbers):
+        if target-num in dic:
+            return [dic[target-num]+1, i+1]
+        dic[num] = i
+
+# binary search
+def twoSum(self, numbers, target):
+    for i in xrange(len(numbers)):
+        l, r = i+1, len(numbers)-1
+        tmp = target - numbers[i]
+        while l <= r:
+            mid = l + (r-l)//2
+            if numbers[mid] == tmp:
+                return [i+1, mid+1]
+            elif numbers[mid] < tmp:
+                l = mid+1
+            else:
+                r = mid-1

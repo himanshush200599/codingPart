4ce8477075cc5b34a3f7878ca58a49bf65044324
himanshush200599 <himanshush214@gmail.com>
2019-01-13 12:26:54 -0500

    inorder iterative solution

---
+++ Algorithm/Tree/TreeTraversal.py
@@ -0,0 +1,28 @@
+class TreeNode(object):
+    """docstring for TreeNode."""
+    def __init__(self, val):
+        super(TreeNode, self).__init__()
+        self.val = val
+        self.left = None
+        self.right = None
+class Traversal(object):
+    def inorder(self,root):
+        if root is None:
+            return
+        st = []
+        while root is not None or len(st)>0:
+            if root is not None:
+                st.append(root)
+                root = root.left
+            else:
+                root = st.pop(-1)
+                print(root.val,end=' ')
+                root = root.right
+        print()
+a = TreeNode(1)
+a.left = TreeNode(3)
+a.left.left = TreeNode(6)
+a.left.right = TreeNode(7)
+a.right = TreeNode(5)
+b = Traversal()
+b.inorder(a)

b8a3f499e9aa8f384a2244061b6f17acfbd0997f
himanshush200599 <himanshush214@gmail.com>
2019-01-13 12:37:02 -0500

    iterative preorder and postorder traversal

--- Algorithm/Tree/TreeTraversal.py
+++ Algorithm/Tree/TreeTraversal.py
@@ -19,6 +19,33 @@ class Traversal(object):
                 print(root.val,end=' ')
                 root = root.right
         print()
+    def preorder(self,root):
+        if root is None:
+            return
+        st = []
+        while root is not None or len(st)>0:
+            if root is not None:
+                print(root.val,end=' ')
+                st.append(root)
+                root = root.left
+            else:
+                root = st.pop(-1)
+                root = root.right
+        print()
+    def postorder(self,root):
+        if root is None:
+            return
+        st = []
+        st2 = []
+        while root is not None or len(st)>0:
+            if root is not None:
+                st2.append(root.val)
+                st.append(root)
+                root = root.right
+            else:
+                root = st.pop(-1)
+                root = root.left
+        print(st2[::-1])
 a = TreeNode(1)
 a.left = TreeNode(3)
 a.left.left = TreeNode(6)
@@ -26,3 +53,5 @@ a.left.right = TreeNode(7)
 a.right = TreeNode(5)
 b = Traversal()
 b.inorder(a)
+b.preorder(a)
+b.postorder(a)

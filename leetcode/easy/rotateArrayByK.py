7697874222c4a39daaee4c3a5c07162eeaf92ace
himanshush200599 <himanshush214@gmail.com>
2019-01-20 07:40:49 -0500

    optimised solution

--- leetcode/easy/rotateArrayByK.py
+++ leetcode/easy/rotateArrayByK.py
@@ -8,3 +8,8 @@ class Solution(object):
         for i in range(k):
             m = nums.pop(-1)
             nums[:] = [m]+nums
+    #more optimised solution in O(1) space 
+    def rotate1(self, nums, k):
+        n = len(nums)
+        k = k % n
+        nums[:] = nums[n-k:] + nums[:n-k]

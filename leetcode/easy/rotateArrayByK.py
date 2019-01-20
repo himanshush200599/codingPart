231a2f278e972fc17b6e86b4ab93503a98d7acb7
himanshush200599 <himanshush214@gmail.com>
2019-01-20 07:27:47 -0500

    Rotate array by k and return (Brute force need optimisation)

---
+++ leetcode/easy/rotateArrayByK.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            m = nums.pop(-1)
+            nums[:] = [m]+nums

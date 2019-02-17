482dd888e7d794bb9f49ea1525e0a908c0cb55a7
himanshush200599 <himanshush214@gmail.com>
2019-02-17 09:59:16 -0500

    Invert a tree using iteration method

--- leetcode/easy/invertTree.py
+++ leetcode/easy/invertTree.py
@@ -10,3 +10,11 @@ class Solution(object):
         if root:
             root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
             return root
+    def invertTreeIterative(self, root):
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        if node:
+            node.left, node.right = node.right, node.left
+            stack += node.left, node.right
+    return root

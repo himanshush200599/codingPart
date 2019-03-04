0abb1e67d7d0fc5d5f3e6fdc0174ee34abdfea4f
himanshush200599 <himanshush214@gmail.com>
2019-03-04 03:20:54 -0500

    Anagram check using set without sorting

--- leetcode/easy/anagram.py
+++ leetcode/easy/anagram.py
@@ -9,3 +9,28 @@ class Solution(object):
         :rtype: bool
         """
         return sorted(s)==sorted(t)
+#efficient way by using set
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)==0 and len(t) ==0:
+          return True
+        elif len(s)==len(t):
+          p=set(s)
+          q=set(t)
+
+
+          if p==q:
+            for i in p:
+                if s.count(i)==t.count(i):
+                     return True
+                else:
+                     return False
+          else:
+             return False
+        else:
+            return False

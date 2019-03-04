8e359ff9c6b91700b0aa14029efbc13e454b2c9a
himanshush200599 <himanshush214@gmail.com>
2019-03-04 02:09:06 -0500

    anagram by sorting both the strings

---
+++ leetcode/easy/anagram.py
@@ -0,0 +1,11 @@
+# this is naive approach where
+# we are checking by sorting both the strings
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s)==sorted(t)

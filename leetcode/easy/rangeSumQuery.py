08e8471c87b858819b186d8e4d98707119b5f978
himanshush200599 <himanshush214@gmail.com>
2019-03-16 09:10:38 -0400

    Efficient version using CacheAray

--- leetcode/easy/rangeSumQuery.py
+++ leetcode/easy/rangeSumQuery.py
@@ -19,3 +19,23 @@ class NumArray(object):
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(i,j)
+
+
+# efficint solution from leetcode
+class NumArray(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.cml = [0] * (len(nums) + 1)
+        for i in range(len(nums)):
+            self.cml[i + 1] = self.cml[i] + nums[i]
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.cml[j + 1] - self.cml[i]

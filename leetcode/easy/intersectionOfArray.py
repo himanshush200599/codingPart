224e2f02511a1ebd9dc7239cf00eb7f0fe89979d
himanshush200599 <himanshush214@gmail.com>
2019-03-18 14:39:43 -0400

    My approach using loops

--- leetcode/easy/intersectionOfArray.py
+++ leetcode/easy/intersectionOfArray.py
@@ -2,4 +2,11 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1) & set(nums2))
 
-    
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        l = []
+        for i in nums1:
+            if i in nums2:
+                l.append(i)
+        return list(set(l))

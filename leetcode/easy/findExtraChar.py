6d3d1dadefabe9f24706d1f95b7ea8734ef5ee8f
himanshush200599 <himanshush214@gmail.com>
2019-03-26 12:09:42 -0400

    My solution using ascii code

--- leetcode/easy/findExtraChar.py
+++ leetcode/easy/findExtraChar.py
@@ -8,3 +8,15 @@ class Solution(object):
         for c in set(t):
             if t.count(c) > s.count(c):
                 return c
+
+
+
+class Solution(object):
+    def findTheDifference(self, s, t):
+        diff = 0
+        for i in range(len(s)):
+            diff -= ord(s[i])
+            diff += ord(t[i])
+        diff += ord(t[-1])
+        return chr(diff)
+        

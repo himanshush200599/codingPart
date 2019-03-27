bbb9c79c66c0e68ac1ab62140bfeb27bd3427592
himanshush200599 <himanshush214@gmail.com>
2019-03-27 13:59:11 -0400

    Brute force by converting into binary number

---
+++ leetcode/easy/binaryWatch.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def readBinaryWatch(self, num):
+        return ['%d:%02d' % (h, m)
+            for h in range(12) for m in range(60)
+            if (bin(h) + bin(m)).count('1') == num]
+# cheated solution from leetcode

6c369f5188bf37f194caeaef8e246018d4edc1e6
himanshush200599 <himanshush214@gmail.com>
2019-07-06 18:04:33 +0530

    Optimised space complexity

--- AlgoExpert/levenshteinDistance.py
+++ AlgoExpert/levenshteinDistance.py
@@ -17,3 +17,26 @@ def EditDistance(word1,word2):
     return dp[n1][n2]
 
 print(EditDistance("abc","yabd"))
+
+
+# optimised space complexity from O(NM) to O(min(N,M))
+
+def EditDistance(str1,str2):
+    small = str1  if len(str1)<len(str2) else str2
+    big = str1  if len(str1)>len(str2) else str2
+    dpEven = [x for x in range(len(small)+1)]
+    dpOdd = [None for _ in range(len(small)+1)]
+    for i in range(1,len(big)+1):
+        if i%2==0:
+            dpCurrent = dpOdd
+            dpPrevious = dpEven
+        else:
+            dpCurrent = dpEven
+            dpPrevious = dpOdd
+        dpCurrent[0] = i
+        for j in range(len(small)+1):
+            if big[i-1]==small[i-1]:
+                dpCurrent[j] = dpPrevious[j-1]
+            else:
+                dpCurrent[j] = 1+min(dpPrevious[j-1],dpPrevious[j],dpCurrent[j-1])
+    return dpEven[-1] if len(big)%2==0 else dpOdd[-1]
